394631bec6cbccbbee60478d80ba52f605eb53fb
Jose Paolo Ferrer <ferrer_paolo@icloud.com>
2017-12-16 20:57:14 +0800

    Added Hello World code with request for input. Displays input with the system date and time.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// A Hellow World program in C#
+/* Test Comment
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +14,15 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.Write("What is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.Write("\nPress Any Key to Exit...");
+
+            Console.ReadKey(true);
         }
     }
 }
